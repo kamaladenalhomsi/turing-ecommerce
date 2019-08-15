7aef4f6c62228876d876cbaab2596516adccee9e
Kamal Eddin Alhumsi <kamalalhomsi2015@gmail.com>
2019-08-15 16:11:54 +0200

    feat: add axios, activate layouts

---
+++ src/layouts/Main.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    Hello From LAyout
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
--- src/router.js
+++ src/router.js
@@ -9,17 +9,23 @@ export default new Router({
   base: process.env.BASE_URL,
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      path: '',
+      component: () => import('./layouts/Main.vue'),
+      children: [
+        {
+          path: '/',
+          name: 'home',
+          component: Home
+        },
+        {
+          path: '/about',
+          name: 'about',
+          // route level code-splitting
+          // this generates a separate chunk (about.[hash].js) for this route
+          // which is lazy-loaded when the route is visited.
+          component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+        }
+      ]
     }
   ]
 })
