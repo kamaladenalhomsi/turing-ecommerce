5f4cd406ff8cd857aa213a2547c8556e86e10b8b
Kamal Eddin Alhumsi <kamalalhomsi2015@gmail.com>
2019-08-21 14:17:04 +0200

    feat: footer, header style feat: header & footer responsive, chore: add & install font-awesome

---
+++ src/components/Home/FooterSocialLink.vue
@@ -0,0 +1,36 @@
+<template>
+  <a class="footer-social-link" :href="icon">
+    <i :class="icon"></i>
+  </a>
+</template>
+
+<script>
+export default {
+  name: 'footer-social-icon',
+  props: {
+    icon: {
+      type: String
+    },
+    link: {
+      type: String
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.footer-social-link {
+  border-radius: 50%;
+  background-color: $docColorLightGray;
+  width: $footerIconRatio;
+  height: $footerIconRatio;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  transition: 0.3s;
+  &:hover {
+    background-color: $typoColorFuchsia;
+    color: $typeColorWhite;
+  }
+}
+</style>
--- src/layouts/Main.vue
+++ src/layouts/Main.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <template>
-      <b-navbar class="navbar-header">
+      <b-navbar class="navbar-header md:px-20">
         <template slot="brand">
           <b-navbar-item href="/">
             <h2 class="bold navbar-header__logo">SHOPMATE</h2>
@@ -22,12 +22,19 @@
               <div v-on:mouseover="itemHover(department.department_id, index)" slot="trigger">
                 <span>{{ department.name }}</span>
               </div>
-              <div class="container navbar-header__dropdown_container">
-                <b-dropdown-item class="navbar-header__dropdown_group" aria-role="listitem">
-                  <span>{{ department.description }}</span>
-                  <b-button class="custom-filled-button">Call to action</b-button>
+              <div class="container navbar-header__dropdown_container flex flex-wrap">
+                <b-dropdown-item
+                  :custom="true"
+                  class="navbar-header__dropdown_group w-full md:w-1/2"
+                  aria-role="listitem"
+                >
+                  <span class="block w-11/12">{{ department.description }}</span>
+                  <b-button class="m-is-filled-fuchsia">Call to action</b-button>
                 </b-dropdown-item>
-                <div class="flex flex-wrap w-32" v-if="department.categories">
+                <div
+                  class="flex flex-wrap w-full mx-10 md:mx-0 md:w-1/3"
+                  v-if="department.categories"
+                >
                   <b-dropdown-item
                     v-for="category in department.categories"
                     :key="category.category_id"
@@ -40,8 +47,8 @@
                 </div>
                 <div v-else class="flex flex-wrap w-32 navbar-header__loader__wrapper">
                   <b-dropdown-item class="navbar-header__loader" v-for="i in 4" :key="i">
-                    <ContentLoader secondaryColor="#bbbbbb" :width="140" :height="25">
-                      <rect x="0" y="0" rx="3" ry="3" width="140" height="25" />
+                    <ContentLoader secondaryColor="#bbbbbb" :width="140" :height="15">
+                      <rect x="0" y="0" rx="3" ry="3" width="140" height="10" />
                     </ContentLoader>
                   </b-dropdown-item>
                 </div>
@@ -49,7 +56,6 @@
             </b-dropdown>
           </b-navbar-item>
         </template>
-
         <template slot="end">
           <b-navbar-item tag="div">
             <div class="buttons">
@@ -63,23 +69,115 @@
       </b-navbar>
     </template>
     <router-view></router-view>
+    <template>
+      <div class="main-footer md:bottom-0">
+        <div class="container container-90">
+          <div class="flex flex-wrap py-12">
+            <div
+              class="w-full flex flex-wrap justify-center md:justify-start mb-10 md:mb-0 md:w-2/6"
+            >
+              <div class="flex justify-start">
+                <footer-social-link icon="fab fa-instagram"></footer-social-link>
+                <footer-social-link icon="fab fa-pinterest"></footer-social-link>
+                <footer-social-link icon="fab fa-twitter"></footer-social-link>
+                <footer-social-link icon="fab fa-facebook-f"></footer-social-link>
+              </div>
+              <h5 class="w-full text-center md:text-left mt-8 c-white f-opensans">©2019 shopmate Ltd</h5>
+            </div>
+            <div class="flex flex-wrap justify-end w-full md:w-4/6">
+              <div
+                class="w-full mb-8 md:mb-0 justify-center md:justify-left md:w-1/3 flex justify-start flex-wrap"
+              >
+                <h4 class="w-full c-white font-bold text-center f-montserrat">QUESTIONS?</h4>
+                <ul class="mt-4 c-white f-opensans text-sm main-footer__list">
+                  <li>
+                    <a href>Help</a>
+                  </li>
+                  <li>
+                    <a href>Track Order</a>
+                  </li>
+                  <li>
+                    <a href>Returns</a>
+                  </li>
+                </ul>
+              </div>
+              <div
+                class="w-full mb-8 md:mb-0 md:w-1/3 justify-center md:justify-left flex justify-start flex-wrap"
+              >
+                <h4 class="w-full c-white font-bold text-center f-montserrat">WHAT'S IN STORE</h4>
+                <ul class="mt-4 c-white f-opensans text-sm main-footer__list">
+                  <li>
+                    <a href>Help</a>
+                  </li>
+                  <li>
+                    <a href>Track Order</a>
+                  </li>
+                  <li>
+                    <a href>Returns</a>
+                  </li>
+                </ul>
+              </div>
+              <div
+                class="w-full mb-8 md:mb-0 md:w-1/3 flex justify-center md:justify-left justify-start flex-wrap"
+              >
+                <h4 class="w-full c-white font-bold text-center f-montserrat">FOLLOW US</h4>
+                <ul class="mt-4 c-white f-opensans text-sm main-footer__list">
+                  <li>
+                    <a href>Who we are</a>
+                  </li>
+                  <li>
+                    <a href>Work with us</a>
+                  </li>
+                  <li>
+                    <a href>Affilate program</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </template>
   </div>
 </template>
 
 <style lang="scss">
+
+.main-footer {
+  background-color: $docColorBlack;
+  width: 100%;
+  position: absolute;
+  padding: 20px 0px;
+  .footer-social-link {
+    margin-left: 20px;
+    &:first-child {
+      margin-left: 0px
+    }
+  }
+  &__list {
+    li {
+      margin-top: 5px;
+      a {
+      transition: 0.3s;
+        &:hover {
+          color: $typoColorFuchsia;
+        }
+      }
+    }
+  }
+}
+
 .navbar-header {
   &__signup {
     margin-right: 30px !important;
   }
   background-color: $docColorFuchsia;
-  padding: 5px 50px;
   &__logo {
     letter-spacing: 9px;
   }
   &__dropdown {
     position: static;
     .dropdown-menu {
-      width: 75%;
       visibility: hidden;
       opacity: 0;
       transition: 0.3s;
@@ -88,14 +186,13 @@
       padding-top: 0px;
       .dropdown-content {
         min-height: 250px;
+        padding-bottom: 20px;
         background-repeat: no-repeat;
-        background-size: cover;
       }
     }
   }
   &__dropdown_container {
     width: 92%;
-    display: flex;
   }
   &__dropdown_wrapper {
     position: static;
@@ -131,7 +228,6 @@
       margin-top: 20px;
     }
     padding-top: 30px;
-    width: 50% !important;
   }
   &__dropdown__Regional {
     .dropdown-content {
@@ -171,6 +267,7 @@
 }
 
 .navbar-item {
+  height: 100%;
   color: $typeColorWhite;
   &:hover {
     color: $typeColorWhite !important;
@@ -194,20 +291,42 @@
 
 .navbar-menu {
   position: relative;
+  background-color: transparent;
 }
 
 .navbar-start {
-  margin-left: 50px;
+  display: flex;
+  height: 100%;
+  align-items: center;
+  margin-left: 20px;
+  margin-bottom: 20px
 }
+// Media Query
+@media screen and (min-width: 1000px) {
+  .navbar-header {
+    &__dropdown {
+      .dropdown-menu {
+        width: 75%;
+        .dropdown-content {
+          background-position-y: -80px;
+          background-size: cover;
+        }
+      }
+    }
+  }
+}
+
 </style>
 
 <script>
 import { ContentLoader } from 'vue-content-loader'
+import FooterSocialLink from '@/components/Home/FooterSocialLink.vue'
 // import FullWidthDropDown from '@/components/custom/FullWidthDropDown.vue';
 export default {
   name: 'main-layout',
   components: {
-    ContentLoader
+    ContentLoader,
+    FooterSocialLink
   },
   async created () {
     this.$_async_query({
