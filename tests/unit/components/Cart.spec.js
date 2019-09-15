be483cafc0fac8a2d46106e0d8a8f0cb92f85786
Kamal Eddin Alhumsi <kamalalhomsi2015@gmail.com>
2019-09-15 11:49:10 +0200

    test: cart & cartTable unit test suite

---
+++ tests/unit/components/Cart.spec.js
@@ -0,0 +1,78 @@
+import { shallowMount } from '@vue/test-utils'
+import Cart from '@/components/Navbar/Cart.vue'
+import store from '@/store/index'
+import { localVue } from '../index'
+import axiosInstance from '@/plugins/axios'
+
+describe('Cart', () => {
+  let vm
+  beforeAll(() => {
+    const wrapper = shallowMount(Cart, {
+      store,
+      localVue,
+      stubs: ['custom-button', 'confirm'],
+      propsData: {
+        model: 'cart',
+        items: []
+      }
+    })
+    vm = wrapper.vm
+    vm.$store.commit('cart/SET_CART_ID', 1)
+  })
+
+  test('it fetch cart items', async () => {
+    let res = [
+      {
+        id: 1
+      }
+    ]
+    axiosInstance.get.mockImplementation(url => {
+      if (url === vm.$rest.SHOPPING_CART.GET(1)) {
+        return Promise.resolve({
+          status: 200,
+          data: res
+        })
+      }
+    })
+    await vm.getCartItems()
+    expect(vm.$store.getters['cart/GET_CART_COUNT']).toEqual(1)
+  })
+  test('it fetch saved items', async () => {
+    let res = [
+      {
+        id: 1
+      }
+    ]
+    axiosInstance.get.mockImplementation(url => {
+      if (url === vm.$rest.SHOPPING_CART.GET_SAVED(1)) {
+        return Promise.resolve({
+          status: 200,
+          data: res
+        })
+      }
+    })
+    await vm.getSavedItems()
+    expect(vm.$store.getters['cart/GET_SAVED_COUNT']).toEqual(1)
+  })
+
+  it('should remove cart item', async () => {
+    vm.$store.commit('cart/SET_CART_ITEMS', [
+      {
+        id: 1
+      }
+    ])
+    axiosInstance.mockImplementation(({ url, method }) => {
+      if (
+        url === vm.$rest.SHOPPING_CART.DELETE_PRODUCT(1) &&
+        method === 'delete'
+      ) {
+        return Promise.resolve({
+          status: 200
+        })
+      }
+    })
+
+    await vm.removeCartItem(1, 0)
+    expect(vm.$store.getters['cart/GET_CART_COUNT']).toEqual(0)
+  })
+})
---
+++ tests/unit/components/CartTable.spec.js
@@ -0,0 +1,67 @@
+import { shallowMount } from '@vue/test-utils'
+import CartTable from '@/components/Navbar/CartTable.vue'
+import store from '@/store/index'
+import { localVue } from '../index'
+import axiosInstance from '@/plugins/axios'
+
+describe('Cart Table', () => {
+  let vm
+  beforeAll(() => {
+    const wrapper = shallowMount(CartTable, {
+      store,
+      localVue,
+      stubs: ['custom-button'],
+      propsData: {
+        model: 'cart',
+        items: []
+      }
+    })
+    vm = wrapper.vm
+  })
+  test('saveForLater method', async () => {
+    store.commit('cart/SET_CART_ITEMS', [
+      {
+        name: 'First Product'
+      }
+    ])
+    store.commit('cart/SET_SAVED_ITEMS', [
+      {
+        name: 'Second Product'
+      }
+    ])
+    axiosInstance.mockImplementation(({ url, method }) => {
+      if (
+        url === vm.$rest.SHOPPING_CART.SAVE_FOR_LATER(1) &&
+        method === 'get'
+      ) {
+        return Promise.resolve({
+          status: 200
+        })
+      }
+    })
+    await vm.saveForLater(1, 0)
+    expect(vm.$store.getters['cart/GET_CART_ITEMS'].length).toEqual(0)
+  })
+
+  test('moveToCart method', async () => {
+    store.commit('cart/SET_CART_ITEMS', [
+      {
+        name: 'First Product'
+      }
+    ])
+    store.commit('cart/SET_SAVED_ITEMS', [
+      {
+        name: 'Second Product'
+      }
+    ])
+    axiosInstance.mockImplementation(({ url, method }) => {
+      if (url === vm.$rest.SHOPPING_CART.MOVE_TO_CART(1) && method === 'get') {
+        return Promise.resolve({
+          status: 200
+        })
+      }
+    })
+    await vm.moveToCart(1, 0)
+    expect(vm.$store.getters['cart/GET_SAVED_COUNT']).toEqual(0)
+  })
+})
