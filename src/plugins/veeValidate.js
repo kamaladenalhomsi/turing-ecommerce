33c975f45c6ba3c0b4cdccd266eb255803cad1ee
Kamal Eddin Alhumsi <kamalalhomsi2015@gmail.com>
2019-09-18 04:49:29 +0200

    feat: split vee-validate custom messages into json files style: delete uneeded file

--- src/plugins/veeValidate.js
+++ src/plugins/veeValidate.js
@@ -1,19 +1,11 @@
 import VeeValidate from 'vee-validate'
 import Vue from 'vue'
+import CustomErrors from '@/constants/veeValidateCustomErrors.json'
+import CustomAttributes from '@/constants/veeValidateCustomAttributes.json'
 const dictionary = {
   en: {
-    custom: {
-      shipping: {
-        required: 'Please choose shipping region!'
-      }
-    },
-    attributes: {
-      address_1: 'Address 1',
-      address_2: 'Address 2',
-      postal_code: 'Postal Code',
-      shipping_region_id: 'Shipping Region',
-      credit_card: 'Credit Card'
-    }
+    custom: CustomErrors,
+    attributes: CustomAttributes
   }
 }
 
